0cd5c745820b1416fa7d2a1ce6f0f8ef26cd28fa
ykm0595 <163884300+ykm0595@users.noreply.github.com>
2026-01-01 11:15:07 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1,22 +1,19 @@
 define Device/sl3000-emmc
   DEVICE_VENDOR := SL
-  DEVICE_MODEL := SL3000
-  DEVICE_VARIANT := EMMC
-
+  DEVICE_MODEL := SL3000 (eMMC)
   DEVICE_DTS := mt7981b-sl3000-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-
   DEVICE_PACKAGES := \
-        kmod-mt7981-firmware kmod-mt76-connac \
-        kmod-mediatek_eth kmod-mediatek_hnat kmod-mt7531 \
-        kmod-mmc kmod-mmc-mtk \
-        block-mount e2fsprogs mkf2fs f2fsck \
-        wireless-regdb wpad-basic-mbedtls
-
-  SUPPORTED_DEVICES := sl3000 sl3000-emmc
-  IMAGE_SIZE := 256m
-
-  IMAGES += sysupgrade.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+	kmod-usb3 \
+	kmod-mt7981-firmware \
+	kmod-leds-gpio \
+	kmod-gpio-button-hotplug \
+	kmod-sdhci-mt7981 \
+	kmod-mmc \
+	kmod-mt7981-eth \
+	kmod-mt7981-wmac \
+	kmod-mediatek_eth \
+	kmod-mediatek_hnat \
+	kmod-mt7531
 endef
 TARGET_DEVICES += sl3000-emmc
